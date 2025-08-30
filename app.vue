a804aa291aa8516b429e623da16901f9de4ddb9f
Sarah <sarah.3anany@gmail.com>
2025-08-30 17:06:09 +0300

    init primevue, tailwind, nuxt

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="app" class="flex justify-center">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primeuix/themes/aura';
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+        '@primevue/nuxt-module',
+        '@nuxtjs/tailwindcss'
+      ],
+      primevue: {
+        options: {
+          theme: {
+            preset: Aura
+          }
+        }
+      }
+})
