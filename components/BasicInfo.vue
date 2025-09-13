ed91e0aca60a9f8ed082ee0dc1b18af2ce3c2aa4
Sarah <sarah.3anany@gmail.com>
2025-09-13 12:12:00 +0300

    InputPhone

--- components/BasicInfo.vue
+++ components/BasicInfo.vue
@@ -1,19 +1,39 @@
 <script setup lang="ts">
 import { zodResolver } from '@primevue/forms/resolvers/zod';
 import { z } from 'zod';
+import InputPhone from '~/components/InputPhone.vue';
 
 const initialValues = ref({
   fullname: '',
   email: '',
   phone: '',
-  // selectedCountry: ""
-
+  selectedCountry: {
+    name: '',
+    phone: '',
+    placeholder: '',
+    flag: ''
+  },
 })
-// const countries = ref<any[]>([]);
-// const res = await fetch(`https://restcountries.com/v3.1/all?fields=name,flags,idd`);
-// const data = await res.json();
+// selectedCountry?: any; 
+const countries = ref<any[]>([]);
+async function getCountries(): Promise<any> {
+  let countries = []
+  const res = await fetch(`https://restcountries.com/v3.1/all?fields=name,flags,idd`);
+  const data = await res.json();
+  for (let i of data) {
+    let temp = { name: "", phone: "", placeholder: "", flag: "" }
+    temp.name = i.name.common
+    temp.phone = i.idd.root + i.idd.suffixes[0]
+    temp.placeholder = temp.phone
+    temp.flag = i.flags.svg
+    countries.push(temp)
+  }
+  return countries
+}
+onMounted(async () => {
+  countries.value = await getCountries()
+})
 // countries.value = data.sort((a: any, b: any) => a.name.common.localeCompare(b.name.common));
-
 const resolver = ref(zodResolver(
   z.object({
     fullname: z.string().min(1, { message: "Full name is required!" })
@@ -24,14 +44,20 @@ const resolver = ref(zodResolver(
     phone: z.string().min(1, { message: "Phone is required!" })
       .min(9, { message: "Phone length must be more than 9 chars." })
       .max(15, { message: "Phone length must be less than 15" }),
+    selectedCountry: z.object({
+      name: z.string(),
+      phone: z.string(),
+      placeholder: z.string(),
+      flag: z.string()
+    }, { message: "Country is required" })
 
   })
 ))
 
 const submitBasicInfo = ({ valid, values }) => {
+  debugger
   if (valid) {
     console.log(valid);
-
   }
 }
 </script>
@@ -50,39 +76,33 @@ const submitBasicInfo = ({ valid, values }) => {
         <Message v-if="$form.email?.invalid" severity="error" size="small" variant="simple">{{
           $form.email.error?.message }}</Message>
       </div>
-      <div class="flex flex-row gap-2 items-center">
-        <!-- <Select
-        v-model="initialValues.country"
-          name="country"
-          :options="countries"
-          optionLabel="name.common"
-          placeholder="Country"
-          filter
-          filterBy="name.common,idd.root,idd.suffixes[0]"
-          filterPlaceholder="Search country..."
-          class="w-1/3"
-        >
-          <template #option="slotProps">
-            <div class="flex items-center gap-2">
-              <img :src="slotProps.option.flags.svg" class="w-5 h-4" />
-              <span>{{ slotProps.option.name.common }} ( {{ slotProps.option.idd.root }}{{ slotProps.option.idd.suffixes[0] }})</span>
-            </div>
-          </template>
-          <template #value="slotProps">
-            <div v-if="slotProps.value" class="flex items-center gap-2">
-              <img :src="slotProps.value.flags.svg" class="w-5 h-4" />
-              <span>{{ slotProps.value.name.common }} ( {{ slotProps.value.idd.root }}{{ slotProps.value.idd.suffixes[0] }})</span>
-            </div>
-            <span v-else>Code</span>
-          </template>
-        </Select> -->
+      <div class="flex flex-col gap-4 w-full">
+        <div class="p-inputgroup">
+          <Select name="selectedCountry" :options="countries" optionLabel="name" filter placeholder="Select country"
+            class="border-right-none">
+            <template #value="slotProps">
+              <div v-if="slotProps.value" class="flex items-center">
+                <img :src="slotProps.value.flag" class="w-5 h-4 mr-2" />
+                <span>{{ slotProps.value.name }}</span>
+                <span class="ml-2 text-500">({{ slotProps.value.phone }})</span>
+              </div>
+              <span v-else class="text-400">Select a country</span>
+            </template>
+            <template #option="slotProps">
+              <div class="country-item flex items-center">
+                <img
+                  :src="slotProps.option.flag"
+                  class="w-5 h-4 mr-2"
+                  alt="flag"
+                />
+                <span>{{ slotProps.option.name }}</span>
+                <span class="ml-2 text-500">({{ slotProps.option.phone }})</span>
+              </div>
+            </template>
+          </Select>
 
-        <InputMask
-          name="phone"
-          placeholder="Phone"
-          mask="(9999) 999-9999"
-          class="flex-1"
-        />
+          <InputText name="phone" style="width: 10rem" type="text" />
+        </div>
       </div>
       <Button type="submit" severity="secondary" label="Submit" />
     </Form>
---
+++ components/InputPhone.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="p-inputgroup">
+    <Select
+      v-model="selectedCountry"
+      :options="model"
+      optionLabel="name"
+      filter
+      style="min-width: 10rem"
+      class="border-right-none"
+      @change="onChange"
+    >
+      <template #value="slotProps">
+        <div v-if="slotProps.value" class="country-item">
+          <img
+            :src="slotProps.value.flag"
+            class="w-5 h-4 mr-2"
+            alt="flag"
+          />
+          <span>{{ slotProps.value.name }}</span>
+          <span class="ml-2 text-500">({{ slotProps.value.phone }})</span>
+        </div>
+        <span v-else class="text-400">Select a country</span>
+      </template>
+      <template #option="slotProps">
+        <div class="country-item flex items-center">
+          <img
+            :src="slotProps.option.flag"
+            class="w-5 h-4 mr-2"
+            alt="flag"
+          />
+          <span>{{ slotProps.option.name }}</span>
+          <span class="ml-2 text-500">({{ slotProps.option.phone }})</span>
+        </div>
+      </template>
+    </Select>
+    <InputText
+      v-model="inputValue.placeholder"
+      style="width: 15rem"
+      
+      type="text"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    model: null,
+  },
+  data() {
+    return {
+      selectedCountry: this.model[0],
+      inputValue: [{name:"",phone:"",placeholder:"",flag:""}],
+    };
+  },
+  methods: {
+    onChange() {
+      this.inputValue = this.selectedCountry;
+    },
+  },
+};
+</script>
+<style scoped>
+.country-item {
+  display: flex;
+  align-items: center;
+}
+</style>
